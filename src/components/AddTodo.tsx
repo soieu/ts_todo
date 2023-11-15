28d0dd911d1dd9004eee7c8e999a3c7762605733
soieu <soieu9898@gmail.com>
2023-11-15 16:43:37 +0900

    Add input, TodoContext, Input by forwardRef

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -4,5 +4,33 @@ import { useTodo } from '../context'
 import { Input } from './Input'
 
 export const AddTodo = () => {
-  return <div>Add To Do</div>
+  const [input, setInput] = useState<string>('')
+  const [todos, setTodos] = useState<string[]>([])
+
+  const handleSubmission = (e: React.FormEvent) => {
+    e.preventDefault()
+    if (input.trim() !== '') {
+      setTodos([...todos, input])
+      setInput('')
+    }
+  }
+  return (
+    <form onSubmit={handleSubmission}>
+      <div className="flex items-center w-full max-w-lg gap-2 p-5 m-auto">
+        <Input
+          value={input}
+          onChange={e => setInput(e.target.value)}
+          type="text"
+          className="w-full px-5 py-2 bg-transparent border-2 outline-none border-zinc-50 rounded-xl placeholder:text-zinc-500 focus:border-white"
+          placeholder="start typing..."
+        />
+        <button
+          type="submit"
+          className="px-5 py-2 text-sm font-normal text-blue-300 bg-blue-900 border-2 border-blue-900 active:scale-95 rounded-xl"
+        >
+          Submit
+        </button>
+      </div>
+    </form>
+  )
 }
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -3,5 +3,5 @@ import { useTodo } from '../context'
 import { SiStarship } from 'react-icons/si'
 
 export const TodoList = () => {
-  return <div>TodoList</div>
+  return <div></div>
 }
